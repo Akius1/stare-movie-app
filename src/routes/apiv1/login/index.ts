bcd9528a1366a9b477bc322fc7b68f8b52f5e6b5
anabiz <anabizconcept9@gmail.com>
2020-11-07 14:10:02 +0100

    user can login

--- src/routes/apiv1/login/index.ts
+++ src/routes/apiv1/login/index.ts
@@ -1,12 +1,35 @@
 import { Request, Response, Router } from "express";
+import passport from "passport";
+import jwt from "jsonwebtoken";
 
 const router = Router();
 
-router.get("/", (_req: Request, res: Response) => {
-  const obj = {
-    data: "Hello world",
-  };
-  res.status(200).json(obj);
+router.post("/", (req: Request, res: Response) => {
+  passport.authenticate("local", function (err, user, info): any {
+    console.log(user);
+    console.log(err);
+    if (err != undefined) {
+      return res.status(500).json({ err });
+    }
+    try {
+      if (user) {
+        console.log(process.env.ACCESS_TOKEN_SECRET);
+        const access_token = jwt.sign(
+          user,
+          `${process.env.ACCESS_TOKEN_SECRET}`,
+          { expiresIn: "10m" },
+        );
+        res.cookie("userData", access_token);
+        return res.status(200).json({
+          accessToken: access_token,
+        });
+      } else {
+        return res.status(401).json(info);
+      }
+    } catch (error) {
+      return res.status(500).json({ error });
+    }
+  })(req, res);
 });
 
 export default router;
