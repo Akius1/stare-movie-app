21443905cd68f8086a65eb388d88d7ac551e8fb7
anabiz <anabizconcept9@gmail.com>
2020-11-07 23:56:46 +0100

    add user and admin authentication

--- src/routes/apiv1/login/index.ts
+++ src/routes/apiv1/login/index.ts
@@ -1,35 +1,8 @@
-import { Request, Response, Router } from "express";
-import passport from "passport";
-import jwt from "jsonwebtoken";
+import { login } from "../../../controller/login";
+import { Router } from "express";
 
 const router = Router();
 
-router.post("/", (req: Request, res: Response) => {
-  passport.authenticate("local", function (err, user, info): any {
-    console.log(user);
-    console.log(err);
-    if (err != undefined) {
-      return res.status(500).json({ err });
-    }
-    try {
-      if (user) {
-        console.log(process.env.ACCESS_TOKEN_SECRET);
-        const access_token = jwt.sign(
-          user,
-          `${process.env.ACCESS_TOKEN_SECRET}`,
-          { expiresIn: "10m" },
-        );
-        res.cookie("userData", access_token);
-        return res.status(200).json({
-          accessToken: access_token,
-        });
-      } else {
-        return res.status(401).json(info);
-      }
-    } catch (error) {
-      return res.status(500).json({ error });
-    }
-  })(req, res);
-});
+router.post("/", login);
 
 export default router;
