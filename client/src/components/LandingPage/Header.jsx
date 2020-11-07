867fbdd3cf00d477f27f158ffbd2c52320439dd4
bondesvick <bondesvick@gmail.com>
2020-11-07 21:35:29 +0100

    minor refactoring

---
+++ client/src/components/LandingPage/Header.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function Header({ setAllFilms }) {
+  function handleChange(params) {
+    //console.log(params.target.value)
+    //setAllFilms()
+  }
+
+  return (
+    <nav className="PageContainer-nav">
+      <NavLink className="PageContainer-logo" to="/">
+        <img className="logo-img" src="./Images/favicon-32x32.png" /> STARE
+      </NavLink>
+      <input
+        className="srch-box"
+        type="search"
+        placeholder="Search..."
+        onChange={handleChange}
+      />
+
+      <div className="auth-nav">
+        <NavLink className="user-nav" to="/register">
+          Sign Up
+        </NavLink>
+        <NavLink className="user-nav" to="/login">
+          Log In
+        </NavLink>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
