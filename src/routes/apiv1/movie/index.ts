d83d952fbf09b0625f06b07874bf02a0b1a8c604
Lawrence Mandong <mandonglawrence@gmail.com>
2020-11-07 19:24:20 +0100

    create-film

--- src/routes/apiv1/movie/index.ts
+++ src/routes/apiv1/movie/index.ts
@@ -1,10 +1,28 @@
 import { Request, Response, Router } from "express";
+import { validateMFilmInfo } from "../../../schema/validateFilmInfo";
+import { getFilmByName, createFilm } from "../../../controller/addFilm";
+import { Row, RowList } from "postgres";
 
 const router = Router();
 
 /* Movie Routes. */
-router.post("/", async function (_req: Request, res: Response) {
-  res.status(200).json({ movie: "Home alone" });
+router.post("/", async function (req: Request, res: Response) {
+  const validFilmInfo = await validateMFilmInfo(req.body);
+  if (validFilmInfo?.error) {
+    res.status(404).json({ error: "Invalid data" });
+  }
+
+  const filmExists: RowList<Row[]> = await getFilmByName(
+    validFilmInfo?.value.name,
+  );
+
+  if (filmExists.count) {
+    return res.json({ message: "Film already exist" });
+  }
+
+  const myFilm = await createFilm(validFilmInfo?.value);
+
+  return res.status(200).json(myFilm);
 });
 
 export default router;
