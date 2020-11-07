c8f3dc34cd071f23f67e18f0976dc2f160716285
anabiz <anabizconcept9@gmail.com>
2020-11-07 23:36:41 +0100

    add user and admin authentication

--- src/routes/apiv1/movie/index.ts
+++ src/routes/apiv1/movie/index.ts
@@ -1,9 +1,14 @@
 import { Request, Response, Router } from "express";
+import { userAuthorization } from "../../../middleware/checkRegisteredUser";
 
 const router = Router();
 
 /* Movie Routes. */
-router.post("/", async function (_req: Request, res: Response) {
+router.post("/", userAuthorization, async function (
+  req: Request,
+  res: Response,
+) {
+  console.log(req);
   res.status(200).json({ movie: "Home alone" });
 });
 
