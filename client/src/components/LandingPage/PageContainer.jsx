c1559e8d781df9ee530479bc70c907268640da0a
bondesvick <bondesvick@gmail.com>
2020-11-10 05:26:20 +0100

    added search and comment funtionality

--- client/src/components/LandingPage/PageContainer.jsx
+++ client/src/components/LandingPage/PageContainer.jsx
@@ -6,11 +6,11 @@ import { useEffect, useState } from "react";
 import { useUser } from "../Login/UserData";
 
 const PageContainer = ({ allFilms }) => {
-  const [data, setData] = useState("");
+  const [data, setData] = useState([]);
   const [ratings, setRating] = useState([]);
 
   useEffect(async () => {
-    const url = "http://localhost:3000/apiv1/films";
+    const url = "https://staremovieapp.herokuapp.com/apiv1/films";
     const response = await fetch(url, {
       method: "GET",
       redirect: "follow",
@@ -24,16 +24,17 @@ const PageContainer = ({ allFilms }) => {
         setData(user1);
       });
   }, []);
+
   const logo = "./Images/favicon-32x32.png";
   return (
     <>
-      <Header logoLink={logo} />
+      <Header logoLink={logo} setFilmData={setData} />
 
       <div className="main-area">
         <div className="grid-wrapper">
-          {data &&
+          {data.length > 0 ? (
             data.map((ten, i) => {
-              console.log(ten);
+              //console.log(ten);
               return (
                 <Display
                   key={i}
@@ -45,11 +46,16 @@ const PageContainer = ({ allFilms }) => {
                   ticket_id={allFilms[i].photo}
                 />
               );
-            })}
+            })
+          ) : (
+            <p>Loading...</p>
+          )}
         </div>
       </div>
 
-      <div className="footer-area"></div>
+      <div className="footer-area">
+        <p className="copyright">STARE Copyright &copy; 2020</p>
+      </div>
     </>
   );
 };
--- client/src/components/SinglePage/Film.jsx
+++ client/src/components/SinglePage/Film.jsx
@@ -1,6 +1,6 @@
 import "./Style.css";
 import React from "react";
-import Header from "./../LandingPage/Header";
+import Header from "./Header";
 import CommentForm from "./CommentForm";
 import Comment from "./Comment";
 
@@ -12,60 +12,61 @@ const Film = ({ film }) => {
       <Header logoLink={logo} />
 
       <div className="main-area">
-        <div className="info-area">
-          <div className="img-info">
-            <img
-              className="single-img"
-              src={film.image_link}
-              alt="chintu-ka-birthday-2020.jpg"
-            />
+        {film ? (
+          <div className="info-area">
+            <div className="img-info">
+              <img
+                className="single-img"
+                src={film.image_link}
+                alt="chintu-ka-birthday-2020.jpg"
+              />
+            </div>
+
+            <div className="text-info">
+              <div>
+                <h3>Nmae: {film.name}</h3>
+              </div>
+
+              <div>
+                <h3>Ticket Price: ${film.ticket_price}</h3>
+              </div>
+
+              <div>
+                <h3>Country: {film.country}</h3>
+              </div>
+
+              <div>
+                <p>Description: {film.description}</p>
+              </div>
+
+              <div>
+                <p>
+                  Genre:{" "}
+                  {film.Genre
+                    ? film.Genre.map((item) => item + " ")
+                    : `loading...`}
+                </p>
+              </div>
+
+              <div>
+                <p>Rating: {film.rating}</p>
+              </div>
+            </div>
           </div>
+        ) : (
+          <p>Loading...</p>
+        )}
 
-          <div className="text-info">
-            <div>
-              <h3>Nmae: {film.name}</h3>
-            </div>
-
-            <div>
-              <h3>Ticket Price: ${film.ticket_price}</h3>
-            </div>
-
-            <div>
-              <h3>Country: {film.country}</h3>
-            </div>
-
-            <div>
-              <p>Description: {film.description}</p>
-            </div>
-
-            <div>
-              <p>
-                Genre:{" "}
-                {film.Genre
-                  ? film.Genre.map((item) => item + " ")
-                  : `loading...`}
-              </p>
-            </div>
-
-            <div>
-              <p>Rating: {film.rating}</p>
-            </div>
-          </div>
-        </div>
         <div className="commentArea">
-          <CommentForm id={film._id} />
+          <CommentForm filmId={film.id} />
 
-          <div>
-            {film.comments ? (
-              <Comment comments={film.comments} />
-            ) : (
-              <p>loading...</p>
-            )}
-          </div>
+          <Comment filmId={film.id} />
         </div>
       </div>
 
-      <div className="footer-area"></div>
+      <div className="footer-area">
+        <p className="copyright">STARE Copyright &copy; 2020</p>
+      </div>
     </>
   );
 };
